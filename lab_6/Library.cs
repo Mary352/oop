bc976ffbabeebc1aba984333fde7da76c8e43521
Mary <mary.sk2000@gmail.com>
2019-11-30 11:22:32 +0300

    -

--- lab_6/Library.cs
+++ lab_6/Library.cs
@@ -4,8 +4,43 @@ using System.Text;
 
 namespace lab_5
 {
-    class Library<T>
+    class Library<PrintEdit>
     {
-        private List<T> list;
+        private List<Book> liBooks;
+        private List<Magazine> liMags;
+        private List<SchoolBook> liSBooks;
+        private static int count = 0;
+
+        public void AddBook(Book b)
+        {
+            liBooks.Add(b);
+        }
+
+        public void AddSBook(SchoolBook sb)
+        {
+            liSBooks.Add(sb);
+            count++;
+        }
+
+        public void AddMag(Magazine m)
+        {
+            liMags.Add(m);
+        }
+
+        public void SeeSomeBooks(int year)
+        {            
+            foreach (Book b in liBooks)
+            {                
+                if (b.Year >= year)
+                {
+                    Console.WriteLine(b);
+                }                
+            }
+        }
+
+        public int CountSBooks()
+        {
+            return count;
+        }
     }
 }
