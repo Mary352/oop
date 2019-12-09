7e29bf7e021c905859f5ce51ae82868cac390ec1
Mary <mary.sk2000@gmail.com>
2019-12-09 08:08:56 +0300

    -

---
+++ lab_10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a_list = new ArrayList();
+
+            a_list.AddRange(new int[] { 5, 10, 23, 108, 4 });
+            a_list.Add("See the difference");
+
+            //Console.WriteLine(a_list.Count);
+        }
+    }
+}
