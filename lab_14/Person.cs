d971e19e36eaf4a587dd2fdc8aa6a718848ba7cc
Mary <mary.sk2000@gmail.com>
2019-12-30 00:43:12 +0300

    -

---
+++ lab_14/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_14
+{
+    [Serializable]
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        
+        public Person(string FirstName, string LastName, int Age)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+            age = Age;
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set => firstName = value;
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set => lastName = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"Имя: {firstName}\nФамилия: {lastName}\nВозраст: {age}\n";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Person a = (Person)obj;
+
+            return (this.FirstName == a.FirstName && this.LastName == a.LastName && this.Age == a.Age);
+
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 211;
+
+            hash = string.IsNullOrEmpty(firstName) ? 0 : firstName.GetHashCode();
+            hash = (hash * 47) + age.GetHashCode();
+            return hash;
+        }
+    }
+}
---
+++ lab_14/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace lab_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //===========================бинарный===================================================================
+            // объект для сериализации 
+            Author a = new Author("Ann", "Smith", 23, "Unique life");
+
+            // создаем объект 
+            BinaryFormatter  formatterBin = new BinaryFormatter(); 
+
+            // получаем поток, куда будем записывать сериализованный объект 
+            using (FileStream fs = new FileStream("points.dat", FileMode.OpenOrCreate)) 
+            { 
+                formatterBin.Serialize(fs, a); 
+            }
+
+            // десериализация 
+            using (FileStream fs = new FileStream("points.dat", FileMode.OpenOrCreate)) 
+            {
+                Author newA =(Author)formatterBin.Deserialize(fs);
+                Console.WriteLine($"FirstName: {newA.FirstName}\nFirstName: {newA.LastName}\nAge:{newA.Age}");
+                Console.WriteLine($"New book: {newA.BookTitle}");
+            }
+
+            //==================SOAP===================================================================================
+
+        }
+
+    }
+}
+
