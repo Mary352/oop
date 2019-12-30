30b1db79cdba7f105d40f56b2d38630fcae0ff38
Mary <mary.sk2000@gmail.com>
2019-12-30 23:08:41 +0300

    -

---
+++ lab_15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+namespace lab_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //========================================1========================================
+
+            Process currentProcess = Process.GetCurrentProcess();
+            Console.WriteLine("ProcessName: " + currentProcess.ProcessName);
+            Console.WriteLine("Id: " + currentProcess.Id);
+            Console.WriteLine("BasePriority: " + currentProcess.BasePriority);
+            Console.WriteLine("StartTime: " + currentProcess.StartTime);
+            Console.WriteLine("SessionId: " + currentProcess.SessionId);
+            Console.WriteLine("UserProcessorTime: " + currentProcess.UserProcessorTime);
+
+            //========================================2========================================
+        }
+    }
+}
